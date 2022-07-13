71e8a60be59dfae1a3b0755b20f575f91272fac6
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-13 16:32:26 +0200

    parte tabella completata

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const gridDom = document.getElementById('grid');
+
+for(let i = 1; i <= 100; i++){
+    const currentElement = createGridSquare(i);
+    currentElement.addEventListener('click', 
+        function() {
+            this.classList.toggle('clicked');
+            console.log(currentElement.textContent);
+        }
+    );
+        gridDom.append(currentElement);
+}
+
+function createGridSquare(number){
+        const currentElement = document.createElement('div');
+        currentElement.classList.add('square');
+        const numberSquare = document.createElement('div');
+        numberSquare.classList.add('square-number');
+        numberSquare.append(number);
+        currentElement.append(number);
+
+        return currentElement;
+    }
