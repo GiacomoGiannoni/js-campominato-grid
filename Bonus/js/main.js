cee687c91cfc4e55c1b97304b3949e9c88faf59e
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-14 11:33:39 +0200

    parte bonus

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -1,24 +1,46 @@
 const generaDom = document.getElementById('genera');
-generaDom.addEventListener('click',
-    function () {
-        const gridDom = document.getElementById('grid');
-        gridDom.innerHTML = '';
-        for(let i = 1; i <= 100; i++){
-            const currentElement = createGridSquare(i);
-            currentElement.addEventListener('click', 
-                function() {
-                this.classList.toggle('clicked');
-                console.log(currentElement.textContent);
-            }
-        );
-        gridDom.append(currentElement);
-    }
-    }
-);
+generaDom.addEventListener('click', startGame );
 
+function startGame() {
+    const gridDom = document.getElementById('grid');
+    gridDom.innerHTML = '';
 
-function createGridSquare(number) {
+    const difficult = parseInt(document.getElementById('difficult').value);
+
+    let cellTotal;
+    let cellPerSide;
+
+    switch (difficult) {
+        case 1:
+        default:
+            cellTotal = 100;
+            break;
+        case 2:
+            cellTotal = 81;
+            break;
+        case 3:
+            cellTotal = 49;
+            break;
+    }
+
+    cellPerSide = Math.sqrt(cellTotal);
+
+    for(let i = 1; i <= cellTotal; i++){
+        const currentElement = createGridSquare(i, cellPerSide);
+        currentElement.addEventListener('click', 
+            function() {
+            this.classList.toggle('clicked');
+            console.log(currentElement.textContent);
+        }
+    );
+    gridDom.append(currentElement);
+}
+}
+
+function createGridSquare(number, cellePerLato) {
         const currentElement = document.createElement('div');
+        currentElement.style.height = `calc(100% / ${cellePerLato})`;
+        currentElement.style.width = `calc(100% / ${cellePerLato})`;
         currentElement.classList.add('square');
         const numberSquare = document.createElement('div');
         numberSquare.classList.add('square-number');
