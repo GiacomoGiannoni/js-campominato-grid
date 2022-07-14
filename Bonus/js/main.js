914eebd6f233ed77c04c64c499409af1f22a88d6
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-14 10:43:06 +0200

    modifiche

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -1,15 +1,21 @@
-const gridDom = document.getElementById('grid');
-
-for(let i = 1; i <= 100; i++){
-    const currentElement = createGridSquare(i);
-    currentElement.addEventListener('click', 
-        function() {
-            this.classList.toggle('clicked');
-            console.log(currentElement.textContent);
-        }
-    );
+const generaDom = document.getElementById('genera');
+generaDom.addEventListener('click',
+    function () {
+        const gridDom = document.getElementById('grid');
+        gridDom.innerHTML = '';
+        for(let i = 1; i <= 100; i++){
+            const currentElement = createGridSquare(i);
+            currentElement.addEventListener('click', 
+                function() {
+                this.classList.toggle('clicked');
+                console.log(currentElement.textContent);
+            }
+        );
         gridDom.append(currentElement);
-}
+    }
+    }
+);
+
 
 function createGridSquare(number) {
         const currentElement = document.createElement('div');
@@ -21,3 +27,4 @@ function createGridSquare(number) {
 
         return currentElement;
 }
+
